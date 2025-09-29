92fac46c22aa397cd0dbd48816fad0f509e246f4
Jason <therawan.jason@gmail.com>
2025-09-29 23:40:49 +0700

    Fix import issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import Header from "./components/header"
 import HeroContent from "./components/hero-content"
 import ShaderBackground from "./components/shader-background"
 import SkillsSection from "./components/skills-section"
-import ProjectsSection from "./components/projects-section"
-import ExperiencesSection from "./components/experiences-section"
-import AchievementsSection from "./components/achievements-section"
 import ContactSection from "./components/contact-section"
 
 export default function App() {
@@ -14,15 +11,6 @@ export default function App() {
       <HeroContent />
       <div id="skills">
         <SkillsSection />
-      </div>
-      <div id="projects">
-        <ProjectsSection />
-      </div>
-      <div id="experience">
-        <ExperiencesSection />
-      </div>
-      <div id="achievements">
-        <AchievementsSection />
       </div>
       <div id="contact">
         <ContactSection />
