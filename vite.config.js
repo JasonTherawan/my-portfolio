ca1153aba085fbccef4888c55a73892d1ef291f9
Jason <therawan.jason@gmail.com>
2025-09-29 23:37:01 +0700

    Add environment variable

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import glsl from 'vite-plugin-glsl'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), glsl()],
+  base: process.env.VITE_BASE_PATH || "/my-portfolio",
 })
